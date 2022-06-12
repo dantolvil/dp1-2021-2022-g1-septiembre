63827f47467d52bbeab781382935e6a462fd96d6
dantolvil <danieltoledovillalba@gmail.com>
2022-06-12 04:14:34 +0200

    Delete TurnsRepository.java

--- src/main/java/org/springframework/samples/parchis_oca/game/TurnsRepository.java
+++
@@ -1,9 +0,0 @@
-package org.springframework.samples.parchis_oca.game;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface TurnsRepository extends CrudRepository<Turns, Integer>{
-
-
-
-}
