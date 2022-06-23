3ef38ae40d13df4a758faabe495ea46c428775c9
dantolvil <danieltoledovillalba@gmail.com>
2022-06-23 23:51:00 +0200

    Subida Controladores Parchis y Oca, update Game
    Añadidios los controladores ParchisController y OcaController. Subida de cambios  para GameController tras añadir los controladores de Parchis y Oca

--- src/main/java/org/springframework/samples/parchis_oca/game/GameController.java
+++ src/main/java/org/springframework/samples/parchis_oca/game/GameController.java
@@ -1,130 +1,118 @@
 package org.springframework.samples.parchis_oca.game;
 
-import java.util.Collection;
-
+import java.util.List;
 import javax.validation.Valid;
-
-import org.springframework.beans.BeanUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.parchis_oca.enums.GameOption;
+import org.springframework.samples.parchis_oca.enums.GameStatus;
 import org.springframework.samples.parchis_oca.player.Player;
 import org.springframework.samples.parchis_oca.player.PlayerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 @Controller
-@RequestMapping("/owners/{ownerId}")
+@RequestMapping("/game")
 public class GameController {
 
-	private static final String VIEWS_PETS_CREATE_OR_UPDATE_FORM = "pets/createOrUpdatePetForm";
+    private static final Logger logger = LoggerFactory.getLogger(GameController.class);
+    private static final String VIEWS_GAME_CREATE_FORM = "game/createGameForm";
+    private static final String VIEWS_GAME_PARCHIS = "game/parchis/";
+    private static final String VIEWS_GAME_OCA = "game/oca/";
+    private static final String VIEWS_JOIN_GAME = "game/joinGameForm";
 
-	private final GameService petService;
-        private final PlayerService ownerService;
 
-	@Autowired
-	public GameController(GameService petService, PlayerService ownerService) {
-		this.petService = petService;
-                this.ownerService = ownerService;
-	}
+    @Autowired
+    private final GameService gameService;
 
-	/*@ModelAttribute("types")
-	public Collection<PetType> populatePetTypes() {
-		return this.petService.findPetTypes();
-	}*/
+    @Autowired
+    private final PlayerService playerService;
 
-	@ModelAttribute("owner")
-	public Player findOwner(@PathVariable("ownerId") int ownerId) {
-		return this.ownerService.findOwnerById(ownerId);
-	}
-        
-        /*@ModelAttribute("pet")
-	public Pet findPet(@PathVariable("petId") Integer petId) {
-            Pet result=null;
-		if(petId!=null)
-                    result=this.clinicService.findPetById(petId);
-                else
-                    result=new Pet();
-            return result;
-	}*/
-                
-	@InitBinder("owner")
-	public void initOwnerBinder(WebDataBinder dataBinder) {
-		dataBinder.setDisallowedFields("id");
-	}
+    @ModelAttribute("games")
+    public List <Game> findAllCreatedGames() {
+        return this.gameService.findGameByStatus(GameStatus.INITIED);
+    }
 
-	/*@InitBinder("pet")
-	public void initPetBinder(WebDataBinder dataBinder) {
-		dataBinder.setValidator(new PetValidator());
-	}*/
 
-	@GetMapping(value = "/pets/new")
-	public String initCreationForm(Player owner, ModelMap model) {
-		Game pet = new Game();
-		//owner.addPet(pet);
-		model.put("pet", pet);
-		return VIEWS_PETS_CREATE_OR_UPDATE_FORM;
-	}
+    @ModelAttribute("player")
+    public Player findPlayer() {
 
-	/*@PostMapping(value = "/pets/new")
-	public String processCreationForm(Owner owner, @Valid Pet pet, BindingResult result, ModelMap model) {		
-		if (result.hasErrors()) {
-			model.put("pet", pet);
-			return VIEWS_PETS_CREATE_OR_UPDATE_FORM;
-		}
-		else {
-                    try{
-                    	owner.addPet(pet);
-                    	this.petService.savePet(pet);
-                    }catch(DuplicatedPetNameException ex){
-                        result.rejectValue("name", "duplicate", "already exists");
-                        return VIEWS_PETS_CREATE_OR_UPDATE_FORM;
-                    }
-                    return "redirect:/owners/{ownerId}";
-		}
-	}*/
+        return this.playerService.getCurrentPlayer().get();
+    }
 
-	@GetMapping(value = "/pets/{petId}/edit")
-	public String initUpdateForm(@PathVariable("petId") int petId, ModelMap model) {
-		Game pet = this.petService.findPetById(petId);
-		model.put("pet", pet);
-		return VIEWS_PETS_CREATE_OR_UPDATE_FORM;
-	}
+    @Autowired
+    public GameController(PlayerService playerService, GameService gameService) {
+        this.playerService = playerService;
+        this.gameService = gameService;
+
+    }
+
+    @GetMapping(value = "/create")
+    public String initCreationForm(ModelMap model) {
+        logger.info("initCreationForm");
+        Game game = new Game();
+        model.put("game", game);
+        return VIEWS_GAME_CREATE_FORM;
+    }
+
+    @GetMapping(value = "/join")
+    public String joinGame(ModelMap model) {
+        return VIEWS_JOIN_GAME;
+    }
+
 
     /**
-     *
-     * @param pet
-     * @param result
-     * @param petId
-     * @param model
-     * @param owner
-     * @param model
-     * @return
+     * method for creating a game.
      */
-        @PostMapping(value = "/pets/{petId}/edit")
-	public String processUpdateForm(@Valid Game pet, BindingResult result, Player owner,@PathVariable("petId") int petId, ModelMap model) {
-		if (result.hasErrors()) {
-			model.put("pet", pet);
-			return VIEWS_PETS_CREATE_OR_UPDATE_FORM;
-		}
-		else {
-                        Game petToUpdate=this.petService.findPetById(petId);
-			BeanUtils.copyProperties(pet, petToUpdate, "id","owner","visits");                                                                                  
-                   /* try {                    
-                        this.petService.savePet(petToUpdate);                    
-                    } catch (DuplicatedPetNameException ex) {
-                        result.rejectValue("name", "duplicate", "already exists");
-                        return VIEWS_PETS_CREATE_OR_UPDATE_FORM;
-                    }*/
-			return "redirect:/owners/{ownerId}";
-		}
-	}
+    @PostMapping(value = "/create")
+    public String processCreationForm(@Valid @ModelAttribute(name = "game") Game game, BindingResult result, @Valid Player player) {
+
+        String new_link;
+        logger.info("createGame " + game.getName());
+
+        if (result.hasErrors()) {
+            return VIEWS_GAME_CREATE_FORM;
+        }
+        if (this.gameService.gameNameExists(game)) {
+            logger.error("ERROR: already exists");
+            result.rejectValue("name", "duplicate", "Already exists!");
+            return VIEWS_GAME_CREATE_FORM;
+        }
+
+        if (player.checkAlreadyCreatedGames()) {
+            logger.error("ERROR: already created");
+            result.rejectValue("name", "already_created", "You already created a game!");
+            return VIEWS_GAME_CREATE_FORM;
+        }
+        else {
+            try {
+                logger.info("creating Gamepieces");
+                this.gameService.setPlayersOfGame(game, player);
+                this.gameService.save(game);
+
+                //this.gameService.initGame(game);
+
+            } catch (Exception ex) {
+                logger.error("ERROR: " + ex.getMessage());
+
+                result.rejectValue("name", "exception", "An unexpected exception occured");
+                return VIEWS_GAME_CREATE_FORM;
+            }
+            new_link = (game.getGameOption() == GameOption.PARCHIS) ? VIEWS_GAME_PARCHIS : VIEWS_GAME_OCA;
+            new_link = new_link + game.getId();
+
+
+
+        }
+        logger.info("redirecting to" + new_link);
+        return "redirect:/" + new_link;
+    }
+
 
 }
---
+++ src/main/java/org/springframework/samples/parchis_oca/game/OcaController.java
@@ -0,0 +1,76 @@
+package org.springframework.samples.parchis_oca.game;
+
+import java.util.Optional;
+import javax.persistence.EntityNotFoundException;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.parchis_oca.player.Player;
+import org.springframework.samples.parchis_oca.player.PlayerService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/game/oca")
+public class OcaController {
+
+    @Autowired
+    OcaService ocaService;
+    
+    @Autowired
+    GameService gameService;
+    
+    @Autowired
+    PlayerService playerService;
+    
+    private static final String VIEWS_GAME = "game/oca";
+    private static final String VIEWS_JOIN_OCA = "game/oca/join/";
+
+    @Autowired
+    public OcaController(GameService gameService, OcaService ocaService, PlayerService playerservice){
+        this.ocaService = ocaService;
+        this.gameService = gameService;
+        this.playerService = playerservice;
+    }
+
+    @GetMapping(value = "{gameid}")
+    public String initCanvasForm(@PathVariable("gameid") String gameid, ModelMap model, HttpServletResponse response) {
+        Optional <Game> game = gameService.findById(gameid);
+
+        //ocaService.initGameBoard(game);
+        return "redirect:/" + VIEWS_JOIN_OCA + gameid;
+    }
+
+    @GetMapping(value = "/join/{gameid}")
+    public String joinOca(@PathVariable("gameid") String gameid, ModelMap model, HttpServletResponse response) throws InterruptedException {
+        response.addHeader("Refresh", "5");
+        Optional < Game > gameOptional = this.gameService.findById(gameid);
+        Game game = gameOptional.orElseThrow(EntityNotFoundException::new);
+        Player player  = playerService.getCurrentPlayer().get();
+
+        model.addAttribute("currentuser", playerService.getCurrentPlayer().get());
+        model.put("game",game);
+
+        return VIEWS_GAME;
+    }
+
+    @GetMapping(value = "/join/{gameid}/quit")
+    public String quitOca(@PathVariable("gameid") int gameid) {
+        //this.gameService.quitGame(gameid);
+        return "redirect:/";
+    }
+
+    @GetMapping(value = "/join/{gameid}/dice")
+    public String diceRole(@PathVariable("gameid") String gameid, ModelMap model, HttpServletResponse response) {
+
+        //check if this is the current user
+        Optional <Game> game = gameService.findById(gameid);
+        //gameService.save(game);
+
+
+        return "redirect:/" + VIEWS_JOIN_OCA + gameid;
+    }
+
+}
