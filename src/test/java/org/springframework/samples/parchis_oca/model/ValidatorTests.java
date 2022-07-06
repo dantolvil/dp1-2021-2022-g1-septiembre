79ba1c78227a9bc9d0a12f83daed44181aaeff23
dantolvil <danieltoledovillalba@gmail.com>
2022-07-06 02:34:51 +0200

    Subida clase de test ValidatorTests

--- src/test/java/org/springframework/samples/parchis_oca/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/parchis_oca/model/ValidatorTests.java
@@ -1,26 +1,22 @@
 package org.springframework.samples.parchis_oca.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
 import java.util.Locale;
 import java.util.Set;
-
 import javax.validation.ConstraintViolation;
 import javax.validation.Validator;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.samples.parchis_oca.user.User;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
-/**
- * @author Michael Isvy Simple test to make sure that Bean Validation is working (useful
- * when upgrading to a new version of Hibernate Validator/ Bean Validation)
- */
 class ValidatorTests {
 
 	private Validator createValidator() {
+		
 		LocalValidatorFactoryBean localValidatorFactoryBean = new LocalValidatorFactoryBean();
 		localValidatorFactoryBean.afterPropertiesSet();
+		
 		return localValidatorFactoryBean;
 	}
 
@@ -40,5 +36,34 @@ class ValidatorTests {
 		assertThat(violation.getPropertyPath().toString()).isEqualTo("firstName");
 		assertThat(violation.getMessage()).isEqualTo("must not be empty");
 	}
+	
+	 @Test
+	    void shouldNotValidateInvalidUserNameAndPassword() {
+
+			User user = new User();
+	        user.setUsername("userTest");
+	        user.setPassword("passTest");
+
+
+	        Validator validator = createValidator();
+			Set<ConstraintViolation<User>> constraintViolations = validator.validate(user);
+
+			assertThat(constraintViolations.size()).isEqualTo(0);
+	    }
+
+	    @Test
+	    void shouldValidateUserNameAndPassword() {
+
+	        User user = new User();
+	        user.setUsername("userTest");
+	        user.setPassword("passTest");
+
+
+	        Validator validator = createValidator();
+	        Set<ConstraintViolation<User>> constraintViolations = validator.validate(user);
+
+	        assertThat(constraintViolations.size()).isEqualTo(0);
+	    }
+
 
 }
