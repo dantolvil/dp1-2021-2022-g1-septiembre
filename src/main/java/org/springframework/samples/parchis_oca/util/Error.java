e9d5d1a7d68bc9f842f286c32f8e94655f985e3d
dantolvil <danieltoledovillalba@gmail.com>
2022-07-17 23:03:22 +0200

    Subida clase error.java
    Subida de la clase error para control de errores en la aplicación.

---
+++ src/main/java/org/springframework/samples/parchis_oca/util/Error.java
@@ -0,0 +1,13 @@
+package org.springframework.samples.parchis_oca.util;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Error
+{
+    String error_message;
+}
