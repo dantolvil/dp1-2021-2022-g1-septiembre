514a83d5624915b299577d4af836e58e9628b51f
dantolvil <danieltoledovillalba@gmail.com>
2022-07-26 22:36:08 +0200

    Delete src/main/java/org/springframework/samples/parchis_oca/user directory

--- src/main/java/org/springframework/samples/parchis_oca/user/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package org.springframework.samples.parchis_oca.user;
-
-import org.springframework.data.repository.CrudRepository;
-
-
-public interface UserRepository extends  CrudRepository<User, String>{
-	
-}
